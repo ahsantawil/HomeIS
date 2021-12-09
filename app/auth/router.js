db9d160c40f4f1eeba0138322bc82e2f94396fe3
ahsantawil <ahsan.tawil07@gmail.com>
2021-12-09 16:02:02 +0700

    create api singin and jwt

--- app/auth/router.js
+++ app/auth/router.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const os = require('os');
-const { signup } = require('./controller');
+const { signup, signin } = require('./controller');
 
 router.post('/signup', multer({dest: os.tmpdir()}).single('image'), signup);
+router.post('/signin', signin);
 
 
 module.exports = router;
